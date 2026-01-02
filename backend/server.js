99cb671bb8c69bc1c6e3dec7e45f54dedd227c79
Jas0016 <jasubajwa00@gmail.com>
2026-01-02 21:42:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
+
 const documentRoutes = require("./routes/documentRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
-
 app.use("/api/documents", documentRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
